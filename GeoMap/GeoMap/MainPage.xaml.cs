ac8224e06521b4ced72c0678604282807e03ce72
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-27 21:34:20 -0300

    App finalizado!

---
+++ GeoMap/GeoMap/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GeoMap
+{
+    public partial class MainPage : ContentPage
+    {
+        public async Task mostrarMapa()
+        {
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            var locationInfo = new Location(location.Latitude, location.Longitude);
+            var options = new MapLaunchOptions { Name = "Meu local" };
+            await Map.OpenAsync(locationInfo, options);
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        async void btnLocalizacao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(10)
+                    });
+                }
+                else
+                {
+                    lblLatitude.Text = $"Latitude: {location.Latitude}";
+                    lblLongitude.Text = $"Longitude: {location.Longitude}";
+                }
+            }
+            catch (FeatureNotSupportedException fns)
+            {
+                // Trata execessões onde não há suporte para GPS
+                await DisplayAlert("Suporte", fns.Message, "OK");
+            }
+            catch (PermissionException pms)
+            {
+                // Trata excessões quando o usuário não permitir a utilização do GPS
+                await DisplayAlert("Permissão", pms.Message, "OK");
+            }
+            catch (Exception exc)
+            {
+                await DisplayAlert("Falha Grave", exc.Message, "OK");
+            }
+        }
+
+        private async void btnMapa_Clicked(object sender, EventArgs e)
+        {
+            await mostrarMapa();
+        }
+    }
+}
